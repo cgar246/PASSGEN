99f3fb317172bc68e02fb5f85bddb6b659a3c0bc
cgar246 <christiang3268@yahoo.com>
2021-08-18 21:06:41 -0700

    adjusted prompt questions

--- script.js
+++ script.js
@@ -20,26 +20,54 @@ var symbols = ["!","@","#","$","%","^","&","*",",",".","?"]
 function generatePassword() {
 
 
-var passwordLength = window.prompt("How many characters would you like to have? (Choose between 8 and 128)");
+var passwordLength = window.prompt("How many characters would you like to have? (Choose between 8 and 128)")
 
 if (passwordLength > 8) {
-  window.alert("Password must be at least 8 to 128 characters.")
-  var passwordLength = window.prompt("Please select 8 to 128 character");
+  window.alert("You've selected 8 or more characters")
 } 
-
+else {
+  window.alert("You've selected less than 8 characters")
+  var passwordLength = window.prompt("Please select 8 to 128 characters");
+}
 if (passwordLength < 128) {
-  window.alert("Password must be at least 8 to 128 characters.")
-  var passwordLength = window.prompt("Please select 8 to 128 characters")
+  window.alert("You've selected 8 or more characters")
+} else {
+  window.alert("You've selected more than 128 characters")
+  var passwordLength = prompt("Please select 8 to 128 characters");
 }
 
-var passworduppercase = window.confirm("Would you like to have uppercase letters")
+var passworduppercase = window.confirm("Would you like to have uppercase letters?")
+
+if (passworduppercase) {
+  window.alert("You have chosen uppercase letters")
+} else {
+  window.alert("You have denied uppercase letters")
+}
 
 var passwordlowercase = window.confirm("Would you like to have lowercase letters")
 
+if (passwordlowercase) {
+  window.alert("You have chosen lowercase letters")
+} else {
+  window.alert("You have denied lowercase letters")
+}
 var passwordnumbers = window.confirm("Would you like to have numbers")
+
+if (passwordnumbers) {
+  window.alert("You have chosen numbers letters")
+} else {
+  window.alert("You have denied numbers letters")
+}
 
 var passwordsymbols = window.confirm("Would you like to have symbols")
 
+if (passwordsymbols) {
+  window.alert("You have chosen symbols letters")
+} else {
+  window.alert("You have denied symbols letters")
 }
+}
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
